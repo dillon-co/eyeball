355ac417c5c721262bf1bc93f00a22245357d6ef
Dillon Cortez <dilloncortez@gmail.com>
2020-10-21 20:01:29 -0600

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
+
+
   resource :payment_calendar, only: :show do
     resources :payment_schedules, only: [:create, :update, :destroy]
   end
     get "cash-flow", to: "payment_callendars#cash_flow"
+  root to: "payment_callendars#cash_flow"
 end
